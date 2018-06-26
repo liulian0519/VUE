f6dcec439feba60acbb547c4e1de6fb39dcee62e
liulian0519 <1223342263@qq.com>
2018-06-26 21:37:01 +0800

    admin

--- pizza-app/src/components/Admin.vue
+++ pizza-app/src/components/Admin.vue
@@ -1,9 +1,39 @@
 <template>
-<h1>Admin</h1>
+<div class="row">
+  <div class="col-sm-12 col-md-8">
+    <!--new pizza-->
+    <app-new-pizza></app-new-pizza>
+  </div>
+  <div class="col-sm-12 col-md-4">
+    <!--品种展示-->
+    <h3 class="text-muted my-5">菜单</h3>
+    <table class="table">
+      <thead class="thead-default">
+      <tr>
+        <th>品种</th>
+        <th>删除</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>榴莲pizza</td>
+        <td>
+          <button class="btn btn-outline-danger btn-sm">&times;</button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
 </template>
 
 <script>
+  import NewPizza from './NewPizza'
     export default {
+    components:{
+
+      'app-new-pizza':NewPizza
+    }
       // data(){
       //     return{
       //       name:"liulian"
---
+++ pizza-app/src/components/NewPizza.vue
@@ -0,0 +1,62 @@
+<template>
+<form>
+  <h3 class="text-muted my-5">添加新的Pizza</h3>
+  <div class="form-group row">
+    <label class="col-sm-1">品种</label>
+    <div class="col-sm-11">
+      <input type="text" class="form-control" v-model="newPizza.name">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-sm-1">描述</label>
+    <div class="col-sm-11">
+      <textarea class="form-control" v-model="newPizza.description" rows="5"></textarea>
+    </div>
+  </div>
+  <p><strong>选项1</strong></p>
+  <div class="form-group row">
+    <label class="col-sm-1">尺寸</label>
+    <div class="col-sm-11">
+      <input type="text" class="form-control" v-model="newPizza.size1">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-sm-1">价格</label>
+    <div class="col-sm-11">
+      <input type="text" class="form-control" v-model="newPizza.price1">
+    </div>
+  </div>
+  <p><strong>选项2</strong></p>
+  <div class="form-group row">
+    <label class="col-sm-1">尺寸</label>
+    <div class="col-sm-11">
+      <input type="text" class="form-control" v-model="newPizza.size2">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-sm-1">价格</label>
+    <div class="col-sm-11">
+      <input type="text" class="form-control" v-model="newPizza.price2">
+    </div>
+  </div>
+  <div class="form-group row mt-5">
+    <button class="btn btn-success btn-block">添加</button>
+  </div>
+
+</form>
+</template>
+
+<script>
+    export default {
+        name: "NewPizza",
+      data(){
+          return{
+            newPizza:{}
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
